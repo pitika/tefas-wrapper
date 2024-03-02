0a226715d7e76fd7a8b12e334bd62dfe30ed6fa3
Serhat Mutlu Durmaz <serhat.md@gmail.com>
2024-03-02 13:55:33 +0300

    pipenv removed and github actions added

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
 import io
-import re
-
 from setuptools import setup
 
 with io.open("README.md", "rt", encoding="utf8") as f:
     readme = f.read()
 
-with io.open("tefaswrapper/__init__.py", "rt", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
-
 setup(
     name="tefas-wrapper",
-    version=version,
+    version='{{VERSION_PLACEHOLDER}}',
     description="Tefas wrapper to fetch funds data from http://tefas.gov.tr/",
     license="MIT",
     long_description=readme,
     long_description_content_type="text/markdown",
-    author="Serhat Durmaz",
+    author="Serhat Mutlu Durmaz",
     author_email="serhat.md@gmail.com",
     url="https://github.com/semudu/tefas-wrapper",
-    download_url="https://github.com/semudu/tefas-wrapper/archive/v" + version + ".tar.gz",
+    download_url="https://github.com/semudu/tefas-wrapper/archive/v{{VERSION_PLACEHOLDER}}.tar.gz",
     keywords=["TEFAS", "WRAPPER", "FUND", "FON"],
-    install_requires=["requests","beautifulsoup4","js2xml","marshmallow","pandas"],
+    install_requires=["requests", "beautifulsoup4", "js2xml", "marshmallow", "pandas"],
     packages=["tefaswrapper"],
     include_package_data=True,
     python_requires=">=3.8",
